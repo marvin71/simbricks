b64fab64f70166ad4e0ccd0029ec5a24ab0204a3
Marvin Meiers <mmeiers@mpi-sws.org>
2024-02-28 16:06:25 +0100

    experiments: merge the NetIf and NicIf e2e classes into one class

--- experiments/simbricks/orchestration/simulators.py
+++ experiments/simbricks/orchestration/simulators.py
@@ -962,7 +962,7 @@ class NS3E2ENet(NetSim):
 
             # add all connected networks
             for c in component.components:
-                if isinstance(c, e2e.E2ESimbricksNetworkNetIf):
+                if isinstance(c, e2e.E2ENetworkSimbricks) and not c.listen:
                     p_suf = ''
                     if c.peer:
                         p_suf = min(c.name, c.peer.name)
@@ -980,10 +980,8 @@ class NS3E2ENet(NetSim):
             for c in component.components:
                 if isinstance(c, e2e.E2ESimbricksHost):
                     self.resolve_socket_paths(env, c)
-                elif isinstance(c, e2e.E2ESimbricksNetworkNetIf):
-                    self.resolve_socket_paths(env, c)
-                elif isinstance(c, e2e.E2ESimbricksNetworkNicIf):
-                    self.resolve_socket_paths(env, c, True)
+                elif isinstance(c, e2e.E2ENetworkSimbricks):
+                    self.resolve_socket_paths(env, c, c.listen)
         if self.use_dce:
             env = old_env
 
